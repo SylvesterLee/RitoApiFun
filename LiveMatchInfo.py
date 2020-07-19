791f8a0d3f9e9b70655e8062a6f5f3e7a79e7aca
SylvesterLee <35133848+SylvesterLee@users.noreply.github.com>
2020-07-20 01:37:04 +0200

    champID to champName, runeID to runeName, collect data of players in a dict. Summoner spells remaining

--- LiveMatchInfo.py
+++ LiveMatchInfo.py
@@ -1,4 +1,5 @@
 from riotwatcher import LolWatcher
+import json
 
 class LiveMatchInfo(object):
     def __init__(self, region, summonerName, api_key):
@@ -10,5 +11,38 @@ class LiveMatchInfo(object):
     def _getEncryptedSummonerName(self):
         return self.lolWatcher.summoner.by_name(self.region, self.summonerName)
 
+    def _champID_to_champName(self, champID):
+        with open('data/champion.json', encoding='utf8') as json_file:
+            data = json.load(json_file)
+            for item in data['data']:
+                if int(data['data'][item]['key']) == champID:
+                    return data['data'][item]['id']
+                    break
+            print(f"Could not find champion with id: {champID}")
+            return 'Null'
+
+    def _runeID_to_runeName(self, runeID):
+        with open('data/runesReforged.json', encoding='utf8') as json_file:
+            data = json.load(json_file)
+            for item in data:
+                for rune in item['slots']:
+                    for run in rune['runes']:
+                        if run['id'] == runeID:
+                            return run['key']
+            print(f"Could not find rune with id: {runeID}")
+            return 'NULL'
+
     def _gameInfo(self):
-        return self.lolWatcher.spectator.by_summoner(self.region, self._getEncryptedSummonerName()['id'])
+        participantList = []
+        liveGameData = self.lolWatcher.spectator.by_summoner(self.region, self._getEncryptedSummonerName()['id'])
+        for participant in liveGameData['participants']:
+            print(participant)
+            currentParticipant = {'team':int(participant['teamId']/100),
+                                  'summonerName':participant['summonerName'],
+                                  'champName':self._champID_to_champName(participant['championId']),
+                                  'keyStone':self._runeID_to_runeName(participant['perks']['perkIds'][0]),
+                                  'runes':[self._runeID_to_runeName(participant['perks']['perkIds'][x]) for x in range(1,6)]
+                                  }
+            participantList.append(currentParticipant)
+
+        return participantList
--- Main.py
+++ Main.py
@@ -4,34 +4,14 @@ from LiveMatchInfo import LiveMatchInfo
 import json
 
 REGION = Consts.REGIONS['euw']
-SUMMONER_NAME = 'FB HolyPhoenîx'
+SUMMONER_NAME = 'IlIlIlIIIlIIIll'
 API_KEY = 'RGAPI-74a53027-2856-414d-870f-44d944107770'
 
 
 def main():
-
-    #response = requests.get('https://127.0.0.1:2999/liveclientdata/allgamedata',verify=False)
-    #print(response.json())
-
     matchInfo = LiveMatchInfo(region=REGION, summonerName=SUMMONER_NAME, api_key=API_KEY)
     print(matchInfo._gameInfo())
 
-    '''
-    champID = 142
-    foundChampName = False
-    with open('Champions.json', encoding='utf8') as json_file:
-        data = json.load(json_file)
-        for item in data['data']:
-            if int(data['data'][item]['key']) == champID:
-                champName = data['data'][item]['id']
-                foundChampName = True
-                break
-        if not foundChampName:
-            print(f"Could not find champion with id: champID")
-            champName = 'Null'
-        else:
-            print(champName)
-            '''
 
 if __name__ == "__main__":
     main()
